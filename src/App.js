9771494c2595a2c2e4dc127565b080d3a3b17006
NureSynenkoIvan <103214936+IvanIKS@users.noreply.github.com>
2025-06-13 19:58:02 +0300

    4е

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import Chats from './pages/Chats';
 import ChatRoom from './pages/ChatRoom';
-
-  const getAuthHeader = () => {
-    const phone = localStorage.getItem('userPhoneNumber');
-    const password = localStorage.getItem('userPassword');
-    return 'Basic ' + btoa(`${phone}:${password}`);
-  };
 
 
 function App() {
